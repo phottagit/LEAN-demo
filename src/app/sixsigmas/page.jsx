d8734584e9aec63bb9be223b0ef984be7ea7ce40
phottagit <piyathida.photijit@gmail.com>
2025-05-02 11:35:08 +0700

    sixsigmapage

--- src/app/sixsigmas/page.jsx
+++ src/app/sixsigmas/page.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function SixSigmaPage() {
   return (
-    <div>
-      
-    </div>
+<div className="flex items-center justify-center h-screen">
+  <h2 className="text-center font-medium text-2xl ">Under construction</h2>
+</div>
   )
 }
 
