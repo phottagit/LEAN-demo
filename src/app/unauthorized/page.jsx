a8c3a9f79a5e43b3b9925977ff96183ca36357e7
phottagit <piyathida.photijit@gmail.com>
2025-05-23 14:04:30 +0700

    up

--- src/app/unauthorized/page.jsx
+++ src/app/unauthorized/page.jsx
@@ -1,6 +1,6 @@
 export default function UnauthorizedPage() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center">
+    <div className="min-h-screen flex flex-col items-center justify-center bg-red-50 text-red-600 text-xl p-10">
       <h2 className="text-center font-medium ">❌ คุณไม่มีสิทธิ์เข้าถึงหน้านี้ (Unauthorized)</h2>
       <h3 className="text-center font-medium ">กรุณาติดต่อผู้ดูแลระบบ</h3>
     </div>
