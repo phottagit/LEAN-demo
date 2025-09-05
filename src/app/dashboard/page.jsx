7dbf70b8aaef18bb299a0d82d7567db6e28d892a
phottagit <piyathida.photijit@gmail.com>
2025-09-05 08:49:42 +0700

    Update page.jsx

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -123,7 +123,7 @@ function DashboardPage() {
     { name: '1', value: 10, type: 'Upper' },
     { name: '2', value: 10, type: 'Upper' },
     { name: '3', value: 10, type: 'Upper' },
-    { name: '4', value: 10, type: 'Upper' },
+    { name: '4', value: 10, type: 'Lower' },
     { name: '5', value: 10, type: '' },
     { name: '6', value: 10, type: '' },
     { name: '7', value: 10, type: '' },
